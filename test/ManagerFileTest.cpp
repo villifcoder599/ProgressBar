d972fd1dd6d96161f4c21ba17e4c8d2e8d30c166
villifCoder559 <francesco.villi@gmail.com>
2021-02-19 18:12:09 +0100

    upgrade exception test

--- test/ManagerFileTest.cpp
+++ test/ManagerFileTest.cpp
@@ -4,33 +4,8 @@
 #include "gtest/gtest.h"
 #include "../ManagerFile.h"
 
-
-TEST(ManagerFileTest, TestLetturaFileCorretti) {
-    auto *mf=new ManagerFile(nullptr);
-    mf->addPath(R"(C:\Users\Francesco Villi\CLionProjects\ProgressBar\test\files\file1.txt)");
-    mf->addPath(R"(C:\Users\Francesco Villi\CLionProjects\ProgressBar\test\files\file2.txt)");
-    mf->LoadFile();
-    EXPECT_EQ(2,mf->getLoadPaths());
-}
-
 TEST(ManagerFileTest,TestLetturaFileErrato){
     auto *mf=new ManagerFile(nullptr);
     mf->addPath(R"(C:\Users\Francesco Villi\CLionProjects\ProgressBar\test\files\fileNotFound.txt)");
-    try{
-        mf->LoadFile();
-    }catch(std::invalid_argument &e){
-        EXPECT_STREQ("file non trovato",e.what());
-    }
-
-}
-
-TEST(ManagerFileTest,TestLetturaPiuDocumenti){
-    auto *mf=new ManagerFile(nullptr);
-    mf->addPath(R"(C:\Users\Francesco Villi\CLionProjects\ProgressBar\test\files\file1.txt)");
-    mf->addPath(R"(C:\Users\Francesco Villi\CLionProjects\ProgressBar\test\files\file2.txt)");
-    mf->LoadFile();
-    ASSERT_EQ(2,mf->getLoadPaths());
-    mf->addPath(R"(C:\Users\Francesco Villi\CLionProjects\ProgressBar\test\files\file3.txt)");
-    mf->LoadFile();
-    ASSERT_EQ(1,mf->getLoadPaths());
+    EXPECT_THROW(mf->LoadFile(),std::invalid_argument);
 }
